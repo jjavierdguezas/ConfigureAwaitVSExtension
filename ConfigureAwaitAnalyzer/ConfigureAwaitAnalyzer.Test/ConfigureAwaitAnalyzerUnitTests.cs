35a443b0622d282acc7f52cfba162dbee55d164f
jj <jjavierdguezas@gmail.com>
2019-04-14 23:56:52 +0200

    add some tests

--- ConfigureAwaitAnalyzer/ConfigureAwaitAnalyzer.Test/ConfigureAwaitAnalyzerUnitTests.cs
+++ ConfigureAwaitAnalyzer/ConfigureAwaitAnalyzer.Test/ConfigureAwaitAnalyzerUnitTests.cs
@@ -4,13 +4,19 @@ using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using TestHelper;
-using ConfigureAwaitAnalyzer;
+using System.Threading;
 
 namespace ConfigureAwaitAnalyzer.Test
 {
     [TestClass]
     public class UnitTest : CodeFixVerifier
     {
+        [TestInitialize]
+        public void TestInitializer()
+        {
+            Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en");
+        }
+
         [TestMethod]
         public void WhenNoCode_ThenNoDiagnostic()
         {
@@ -101,7 +107,7 @@ namespace TestApp
             var expected = new DiagnosticResult
             {
                 Id = "ConfigureAwaitAnalyzer",
-                Message = String.Format("'{0}' call maybe needs to configure an awaiter", "await Test()"),
+                Message = String.Format("Maybe '{0}' call needs to configure an awaiter", "await Test()"),
                 Severity = DiagnosticSeverity.Warning,
                 Locations =
                         new[] {
@@ -148,7 +154,7 @@ namespace TestApp
             var expected = new DiagnosticResult
             {
                 Id = "ConfigureAwaitAnalyzer",
-                Message = String.Format("'{0}' call maybe needs to configure an awaiter", "await Task.Run(() => null)"),
+                Message = String.Format("Maybe '{0}' call needs to configure an awaiter", "await Task.Run(() => null)"),
                 Severity = DiagnosticSeverity.Warning,
                 Locations =
                         new[] {
@@ -196,7 +202,7 @@ namespace TestApp
             var expected = new DiagnosticResult
             {
                 Id = "ConfigureAwaitAnalyzer",
-                Message = String.Format("'{0}' call maybe needs to configure an awaiter", "await task"),
+                Message = String.Format("Maybe '{0}' call needs to configure an awaiter", "await task"),
                 Severity = DiagnosticSeverity.Warning,
                 Locations =
                         new[] {
@@ -243,7 +249,7 @@ namespace TestApp
             var expected1 = new DiagnosticResult
             {
                 Id = "ConfigureAwaitAnalyzer",
-                Message = String.Format("'{0}' call maybe needs to configure an awaiter", "await Task.Run(async () => await Test())"),
+                Message = String.Format("Maybe '{0}' call needs to configure an awaiter", "await Task.Run(async () => await Test())"),
                 Severity = DiagnosticSeverity.Warning,
                 Locations =
                         new[] {
@@ -254,7 +260,7 @@ namespace TestApp
             var expected2 = new DiagnosticResult
             {
                 Id = "ConfigureAwaitAnalyzer",
-                Message = String.Format("'{0}' call maybe needs to configure an awaiter", "await Test()"),
+                Message = String.Format("Maybe '{0}' call needs to configure an awaiter", "await Test()"),
                 Severity = DiagnosticSeverity.Warning,
                 Locations =
                         new[] {
@@ -265,53 +271,100 @@ namespace TestApp
             VerifyCSharpDiagnostic(test, expected1, expected2);
         }
 
-        //Diagnostic and CodeFix both triggered and checked for
-        //    [TestMethod]
-        //    public void TestMethod2()
-        //    {
-        //        var test = @"
-        //using System;
-        //using System.Collections.Generic;
-        //using System.Linq;
-        //using System.Text;
-        //using System.Threading.Tasks;
-        //using System.Diagnostics;
+        
+        [TestMethod]
+        public void WhenThereIsCodeToFix_ThenItGetFixed()
+        {
+            var test = @"
+using System;
+using System.Threading.Tasks;
 
-        //namespace ConsoleApplication1
-        //{
-        //    class TypeName
-        //    {   
-        //    }
-        //}";
-        //        var expected = new DiagnosticResult
-        //        {
-        //            Id = "ConfigureAwaitAnalyzer",
-        //            Message = String.Format("Type name '{0}' contains lowercase letters", "TypeName"),
-        //            Severity = DiagnosticSeverity.Warning,
-        //            Locations =
-        //                new[] {
-        //                        new DiagnosticResultLocation("Test0.cs", 11, 15)
-        //                    }
-        //        };
+namespace ConsoleApp2
+{
+    class Program
+    {
+        private static Task<int> Test()
+        {
+            return Task.FromResult(3);
+        }
 
-        //        VerifyCSharpDiagnostic(test, expected);
+        private static async Task Run()
+        {
+            int a = await Test().ConfigureAwait(false);
+            await Test().ConfigureAwait(false);
+            await Test();
+            await Test();
 
-        //        var fixtest = @"
-        //using System;
-        //using System.Collections.Generic;
-        //using System.Linq;
-        //using System.Text;
-        //using System.Threading.Tasks;
-        //using System.Diagnostics;
+            await Task.Run(() => null);
+            await Task.Run(async () => await Test());
 
-        //namespace ConsoleApplication1
-        //{
-        //    class TYPENAME
-        //    {   
-        //    }
-        //}";
-        //        VerifyCSharpFix(test, fixtest);
-        //    }
+            var task = Test();
+            var i = await task;
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Run().Wait();
+            }
+            catch
+            {
+            }
+
+        }
+    }
+}
+";
+            
+
+            var fixtest = @"
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        private static Task<int> Test()
+        {
+            return Task.FromResult(3);
+        }
+
+        private static async Task Run()
+        {
+            int a = await Test().ConfigureAwait(false);
+            await Test().ConfigureAwait(false);
+            await Test().ConfigureAwait(false);
+            await Test().ConfigureAwait(false);
+
+            await Task.Run(() => null).ConfigureAwait(false);
+            await Task.Run(async () => await Test().ConfigureAwait(false)).ConfigureAwait(false);
+
+            var task = Test();
+            var i = await task.ConfigureAwait(false);
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Run().Wait();
+            }
+            catch
+            {
+            }
+
+        }
+    }
+}
+";
+            VerifyCSharpFix(test, fixtest);
+        }
 
         protected override CodeFixProvider GetCSharpCodeFixProvider()
         {
